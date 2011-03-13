328c294a2c9f9fea3bc3c55a6ea1987115a008d2
Jeff Balogh <jbalogh@mozilla.com>
2011-03-13 16:31:00 -0700

    only run a single iteratePings so we cross the interval barrier gracefully
    With multiple iteratePings running one would clearRect over the other's
    paint. Now there's a single global array of pings and any animations still
    running get carried over when a new interval starts.

--- media/map.js
+++ media/map.js
@@ -1,7 +1,9 @@
 function initMap() {
     "use strict";
     var scale = $mc.width() / 3600,
-        ctx = $("#pings")[0].getContext("2d");
+        ctx = $("#pings")[0].getContext("2d"),
+        currentData = [],
+        start = 0, total = 0, duration = 0, extra = 0;
 
     glow.map = {};
 
@@ -26,70 +28,91 @@ function initMap() {
             /* [date, total, [pings]] */
             data = response.data,
             pings = data[2],
-            currentData = [];
+            newData = [];
 
-        /* Add `count` [lat, long] pairs to currentData, then shuffle the whole
+        /* Add `count` [lat, long] pairs to newData, then shuffle the whole
          * thing. */
         for (var j = 0, jj = pings.length; j < jj; j++) {
-            // My kingdom for destructuring bind.
             var ping = pings[j],
                 latitude = ~~((parseFloat(ping[0]) + 180) * 10 * scale),
                 longitude = ~~((-parseFloat(ping[1]) + 90) * 10 * scale),
                 count = ping[2];
             for (var k = 0; k < count; k++) {
-                currentData.push([0, latitude, longitude]);
+                newData.push([0, latitude, longitude]);
             }
         }
-        shuffle(currentData);
+        shuffle(newData);
+
+        /* Ping animations last 1 second so some are going to roll over. This
+         * updates the time left to work with the reset timer and adds them to
+         * the front of newData. */
+        extra = Math.max(0, currentData.length - 1 - start);
+        if (extra > 0) {
+            for (var i = start, ii = currentData.length; i < ii; i++) {
+                currentData[i][0] = currentData[i][0] - response.interval;
+            }
+            newData.unshift.apply(newData, currentData.slice(start));
+        }
+
+        /* Reset all the globals and kick off a new animation interval. */
+        currentData = newData;
+        start = 0;
+        total = currentData.length;
+        duration = response.interval * 1000;
+        dbg('map total', total);
 
         glow.fetchMap(response.interval);
-        setTimeout(glow.map.playNext, response.interval * 1000);
+        vast.animate.over(response.interval * 1000, iteratePings, this,
+                          {after: glow.map.playNext});
+    };
 
-        var start = 0,
-            total = currentData.length,
-            duration = response.interval * 1000,
-            reg = vast.GlobalClock.register(iteratePings, this);
 
-        function iteratePings(t) {
-            if (start == total) {
-                reg.die();
-            }
-            // How far along the current interval we are [0...1].
-            var tt = Math.min(1, (t - reg.start) / duration),
-                i, ping, age, end;
+    function iteratePings(t) {
+        // t marks our progress through the interval from 0..1.
+        if (start == total) {
+            return;
+        }
+        /* We ignore the extra items when figuring out how many new pings to
+         * play this iteration. The extra items get picked up as start moves
+         * along. */
+        var end = extra + (t * (total - extra)),
+            currentAge = -1,
+            progress = t * (duration / 1000),
+            i, ping, age;
 
-            // Clear out the canvas.
-            for (i = start, end = tt * total; i < end; i++) {
-                ping = currentData[i];
-                ctx.clearRect(ping[1] - 5, ping[2] - 5, 10, 10);
-            }
+        // Clear out the canvas.
+        for (i = start; i < end; i++) {
+            ping = currentData[i];
+            ctx.clearRect(ping[1] - 5, ping[2] - 5, 10, 10);
+        }
 
-            var currentAge = -1;
-            for (i = start, end = tt * total; i < end; i++) {
-                ping = currentData[i];
-                if (ping[0] == 0) {
-                    ping[0] = t;
+        for (i = start; i < end; i++) {
+            ping = currentData[i];
+            if (ping[0] == 0) {
+                ping[0] = progress;
+            } else {
+                age = progress - ping[0];
+                if (age > 1) {
+                    // This ping has played out, don't come back to it.
+                    start++;
                 } else {
-                    age = (t - ping[0]) / 1000;
-                    if (age > 1) {
-                        // This ping has played out, don't come back to it.
-                        start++;
-                    } else {
-                        /* Setting fillStyle is expensive according to Chrome's
-                         * profiler, so only set it if the fillStyle has
-                         * changed. */
-                        if (age != currentAge) {
-                            ctx.fillStyle = "rgba(255, 255, 255, " + (1 - age) + ")";
-                            currentAge = age;
-                        }
-                        ctx.beginPath();
-                        ctx.arc(ping[1], ping[2], 5 * age, 0, Math.PI * 2);
-                        ctx.fill();
+                    /* Setting fillStyle is expensive according to Chrome's
+                     * profiler, so only set it if the fillStyle has
+                     * changed. */
+                    if (age != currentAge) {
+                        /* Bound the alpha between 0.01 and .99 so JavaScript
+                         * doesn't write scientific notation. */
+                        ctx.fillStyle = "rgba(255, 255, 255, " +
+                                        Math.max(.01, Math.min(.99, (1 - age))) + ")";
+                        currentAge = age;
                     }
+                    ctx.beginPath();
+                    ctx.arc(ping[1], ping[2], 5 * age, 0, Math.PI * 2);
+                    ctx.fill();
                 }
             }
         }
-    };
+    }
 
     $(window).resize(vast.debounce(function() {
         $("#pings").css({

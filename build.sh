fdd66ccebbedd3427f85da47b102ca8ab2ca0cfd
Jeff Balogh <jbalogh@mozilla.com>
2011-03-14 11:55:21 -0700

    write min.js to the cwd

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 # ./build.sh creates media/glow.min.js for minified production javascript.
 
-java -jar bin/compiler.jar $(grep 'script src.*\.js"' index.html | perl -pi -e 's/.*src="(.*)".*/--js="\1"/') --js_output_file=media/glow.min.js
+java -jar bin/compiler.jar $(grep 'script src.*\.js"' index.html | perl -pi -e 's/.*src="(.*)".*/--js="\1"/') --js_output_file=glow.min.js
